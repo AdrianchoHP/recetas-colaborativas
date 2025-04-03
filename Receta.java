3a144790318218dbc86a6383c74bd201aa688604
MarlonY777 <sanchezmoncada.marlonyesid2017@gmail.com>
2025-04-03 06:58:25 -0500

    Feat Marlon: Agrega primera parte del modelo de receta

---
+++ Receta.java
@@ -0,0 +1,12 @@
+public class Receta {
+    private String nombre;
+    private String[] ingredientes;
+    private String instrucciones;
+    
+    public Receta(String nombre, String[] ingredientes, String instrucciones) {
+        this.nombre = nombre;
+        this.ingredientes = ingredientes;
+        this.instrucciones = instrucciones;
+    }
+
+}
