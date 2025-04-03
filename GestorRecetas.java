d2b42bc68367c9599cefb292bd8db238a9769885
DiegoCandela <diegocandela0128@gmail.com>
2025-04-03 06:57:24 -0500

    feat: agrega gestión de recetas 1

---
+++ GestorRecetas.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class GestorRecetas {
+    private List<Receta> recetas = new ArrayList<>();
+    
+    public void agregarReceta(Receta receta) {
+        recetas.add(receta);
+    }
+    
+}
